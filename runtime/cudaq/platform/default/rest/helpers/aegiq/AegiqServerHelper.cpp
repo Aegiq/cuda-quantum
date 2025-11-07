c33d9ebccf674ee54b3a5971e72ea016fc98199b
Harry Bromley <harry.bromley@aegiq.com>
2025-11-07 14:13:43 +0000

    Split server helper functions and added more extensive initialization
    Signed-off-by: Harry Bromley <harry.bromley@aegiq.com>

--- runtime/cudaq/platform/default/rest/helpers/aegiq/AegiqServerHelper.cpp
+++ runtime/cudaq/platform/default/rest/helpers/aegiq/AegiqServerHelper.cpp
@@ -13,12 +13,8 @@
 #include "cudaq/utils/cudaq_utils.h"
 #include <bitset>
 #include <fstream>
-#include <iostream>
 #include <map>
-#include <regex>
-#include <sstream>
 #include <thread>
-#include <unordered_set>
 
 using json = nlohmann::json;
 
@@ -34,118 +30,182 @@ class AegiqServerHelper : public ServerHelper {
 public:
   const std::string name() const override { return "aegiq"; }
 
+  /// @brief Creates backend with provided configuration.
+  void initialize(BackendConfig config) override;
+
   /// @brief Define required headers.
-  RestHeaders getHeaders() override {
-    RestHeaders headers;
-    headers["Content-Type"] = "application/json";
+  RestHeaders getHeaders() override;
 
-    // Add authentication key
-    if (backendConfig.count("api_key"))
-      headers["x-access-token"] = backendConfig["api_key"];
-
-    return headers;
-  }
-
-  /// @brief Example implementation of backend initialization.
-  void initialize(BackendConfig config) override {
-    CUDAQ_INFO("Initializing Provider Name Backend");
-    backendConfig = config;
-
-    if (!backendConfig.count("url"))
-      backendConfig["url"] = DEFAULT_URL;
-    if (!backendConfig.count("version"))
-      backendConfig["version"] = DEFAULT_VERSION;
-
-    // Set shots if provided
-    if (config.find("shots") != config.end())
-      this->setShots(std::stoul(config["shots"]));
-  }
-
-  /// Generate required jobs from a list of jobs
-  ServerJobPayload createJob(std::vector<KernelExecution> &circuitCodes) override {
-    std::vector<ServerMessage> jobs;
-    for (auto &circuitCode : circuitCodes) {
-      ServerMessage job;
-      job["qpu"] = "Artemis"; // This should probably be set somewhere!
-      job["name"] = circuitCode.name;
-      job["lightworks_version"] = "0.0.0"; // This is not important here
-      job["shots"] = shots;
-      // Below this line need to figure out how these would be defined.
-      job["input"] = NULL;
-      job["min_direction"] = NULL;
-      job["direction_implementation"] = NULL;
-      job["unitary"] = NULL;
-      job["circuit_spec"] = circuitCode.code; // Does this make sense? Will need to be converted to correct format
-      job["job_data"] = NULL;
-
-      jobs.push_back(job);
-    }
-
-    RestHeaders headers = getHeaders();
-    std::string path = "/jobs";
-
-    return std::make_tuple(backendConfig["url"] + path, headers, jobs);
-  }
+  /// @brief Generates required jobs from a vector of jobs
+  ServerJobPayload createJob(std::vector<KernelExecution> &circuitCodes) override;
 
   /// @brief Example implementation of job ID tracking.
-  std::string extractJobId(ServerMessage &postResponse) override {
-    if (!postResponse.contains("X-Job-Id"))
-      return "";
-
-    return postResponse.at("X-Job-Id");
-  }
+  std::string extractJobId(ServerMessage &postResponse) override;
 
   /// @brief Example implementation of job ID tracking.
-  std::string constructGetJobPath(ServerMessage &postResponse) override {
-    return extractJobId(postResponse);
-  }
+  std::string constructGetJobPath(ServerMessage &postResponse) override;
 
   /// @brief Track job based on the ID.
-  std::string constructGetJobPath(std::string &jobId) override {
-    return backendConfig["url"] + "/jobs/" + jobId;
-  }
+  std::string constructGetJobPath(std::string &jobId) override;
 
   /// @brief Example implementation of job status checking.
-  bool jobIsDone(ServerMessage &getJobResponse) override {
-    if (!getJobResponse.contains("status"))
-      return false;
+  bool jobIsDone(ServerMessage &getJobResponse) override;
 
-    std::string status = getJobResponse["status"];
-    return status == "COMPLETED" || status == "FAILED";
-  }
-
-  /// @brief Example implementation of result processing.
-  ///
-  /// The raw results from quantum hardware often need post-processing (bit
-  /// reordering, normalization, etc.) to match CUDA-Q's expectations.
-  /// This is the place to do that.
+  /// @brief Proccess returned results and gets them in to the required format
   cudaq::sample_result processResults(ServerMessage &getJobResponse,
-                                     std::string &jobId) override {
-    CUDAQ_INFO("Processing results: {}", getJobResponse.dump());
+                                      std::string &jobId) override;
 
-    // Extract measurement results from the response
-    auto samplesJson = getJobResponse["results"]["counts"];
-    cudaq::CountsDictionary counts;
-
-    for (auto &item : samplesJson.items()) {
-      std::string bitstring = item.key();
-      std::size_t count = item.value();
-      counts[bitstring] = count;
-    }
-
-    // Create an ExecutionResult
-    cudaq::ExecutionResult execResult{counts};
-
-    // Return the sample_result
-    return cudaq::sample_result{execResult};
-  }
-
-  /// @brief Example implementation of polling configuration.
+  /// @brief Sets polling interval for system.
   std::chrono::microseconds
   nextResultPollingInterval(ServerMessage &postResponse) override {
-    return std::chrono::seconds(5);
+    return std::chrono::seconds(1);
   }
+
+private:
+  /// @brief Helper method to retrieve the value of an environment variable.
+  std::string getEnvVar(const std::string &key, const std::string &defaultVal,
+                        const bool isRequired) const;
+
+  /// @brief Helper function to get value from config or return a default value.
+  std::string getValueOrDefault(const BackendConfig &config,
+                                const std::string &key,
+                                const std::string &defaultValue) const;
 };
+                    
+
+
+/// @brief Example implementation of backend initialization.
+void AegiqServerHelper::initialize(BackendConfig config) {
+  CUDAQ_INFO("Initializing Provider Name Backend");
+
+  backendConfig = config;
+
+  backendConfig["url"] = getValueOrDefault(config, "url", DEFAULT_URL);
+  backendConfig["version"] = DEFAULT_VERSION;
+  
+  /// @todo Decide if/how default qpu should be set
+  backendConfig["qpu"] = getValueOrDefault(config, "qpu", "Artemis");
+
+  backendConfig["api_key"] = getEnvVar("AEGIQ_API_KEY", 0, true);
+
+  // Set shots if provided
+  if (config.find("shots") != config.end())
+    this->setShots(std::stoul(config["shots"]));
+}
+
+/// @brief Define required headers.
+RestHeaders AegiqServerHelper::getHeaders() {
+  RestHeaders headers;
+  headers["Content-Type"] = "application/json";
+
+  // Add authentication key
+  if (backendConfig.count("api_key"))
+    headers["x-access-token"] = backendConfig["api_key"];
+
+  return headers;
+}
+
+/// Generate required jobs from a list of jobs
+ServerJobPayload AegiqServerHelper::createJob(std::vector<KernelExecution> &circuitCodes) {
+  std::vector<ServerMessage> jobs;
+  for (auto &circuitCode : circuitCodes) {
+    ServerMessage job;
+    job["qpu"] = backendConfig["qpu"];
+    job["name"] = circuitCode.name;
+    job["lightworks_version"] = "0.0.0"; // This is not important here so just get to be generic
+    job["n_samples"] = shots;
+    // Below this line need to figure out how these would be defined.
+    job["input"] = NULL;
+    job["min_direction"] = NULL;
+    job["direction_implementation"] = NULL;
+    job["unitary"] = NULL;
+    job["circuit_spec"] = circuitCode.code; // Does this make sense? Will need to be converted to correct format
+    job["job_data"] = NULL;
+
+    jobs.push_back(job);
+  }
+
+  RestHeaders headers = getHeaders();
+  std::string path = "/jobs";
+
+  return std::make_tuple(backendConfig["url"] + path, headers, jobs);
+}
+
+/// @brief Example implementation of job ID tracking.
+std::string AegiqServerHelper::extractJobId(ServerMessage &postResponse) {
+  if (!postResponse.contains("X-Job-Id"))
+    return "";
+
+  return postResponse.at("X-Job-Id");
+}
+
+/// @brief Example implementation of job ID tracking.
+std::string AegiqServerHelper::constructGetJobPath(ServerMessage &postResponse) {
+  return extractJobId(postResponse);
+}
+
+/// @brief Track job based on the ID.
+std::string AegiqServerHelper::constructGetJobPath(std::string &jobId) {
+  return backendConfig["url"] + "/jobs/" + jobId;
+}
+
+/// @brief Example implementation of job status checking.
+bool AegiqServerHelper::jobIsDone(ServerMessage &getJobResponse) {
+  if (!getJobResponse.contains("status"))
+    return false;
+
+  std::string status = getJobResponse["status"];
+  return status == "COMPLETED" || status == "FAILED";
+}
+
+/// @brief Example implementation of result processing.
+///
+/// The raw results from quantum hardware often need post-processing (bit
+/// reordering, normalization, etc.) to match CUDA-Q's expectations.
+/// This is the place to do that.
+cudaq::sample_result AegiqServerHelper::processResults(ServerMessage &getJobResponse,
+                                                       std::string &jobId) {
+  CUDAQ_INFO("Processing results: {}", getJobResponse.dump());
+
+  // Extract measurement results from the response
+  auto samplesJson = getJobResponse["results"]["counts"];
+  cudaq::CountsDictionary counts;
+
+  for (auto &item : samplesJson.items()) {
+    std::string bitstring = item.key();
+    std::size_t count = item.value();
+    counts[bitstring] = count;
+  }
+
+  // Create an ExecutionResult
+  cudaq::ExecutionResult execResult{counts};
+
+  // Return the sample_result
+  return cudaq::sample_result{execResult};
+}
+
+// Helper method to retrieve an environment variable
+std::string AegiqServerHelper::getEnvVar(const std::string &key,
+                                              const std::string &defaultVal,
+                                              const bool isRequired) const {
+  const char *env_var = std::getenv(key.c_str());
+  if (env_var == nullptr) {
+    if (isRequired)
+      throw std::runtime_error("Environment variable " + key +
+                               " is required but not set.");
+    else
+      return defaultVal;
+  }
+  return std::string(env_var);
+}
+
+// Helper function to get a value from config or return a default
+std::string AegiqServerHelper::getValueOrDefault(
+    const BackendConfig &config, const std::string &key,
+    const std::string &defaultValue) const {
+  auto it = config.find(key);
+  return (it != config.end()) ? it->second : defaultValue;
+}
 
 } // namespace cudaq
 
